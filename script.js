12f90f2614fcfae194c3261bf05773bf857d0f84
Basant-spec <basantdhiman9@gmail.com>
2024-11-20 14:14:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function getFormvalue() {
-    //Write your code here
-
+function getFormvalue(event) {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+    
+    // Get the values from the form fields
+    const firstName = document.forms["form1"]["fname"].value;
+    const lastName = document.forms["form1"]["lname"].value;
+    
+    // Combine the first name and last name
+    const fullName = firstName + " " + lastName;
+    
+    // Display the full name using an alert
+    alert(fullName);
 }
